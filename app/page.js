819a235e67d79075148b8ea59b625db0364d2a25
hiep2309 <larvahiep.com>
2024-12-04 00:34:00 +0900

    Hiep

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Page  () {
   <div className="bg-[#D9D9D9] col-span-1 row-span-1 flex items-center justify-center">
     
     Tin tức</div>
-  <div className="bg-[#D9D9D9] flex items-center justify-center">Pháp luật</div>
+  <div className="bg-[#D9D9D9] flex items-center justify-center">Pháp luật ,mfngfdg</div>
   <div className="bg-[#D9D9D9] flex items-center justify-center">Du lịch</div>
   <div className="bg-[#D9D9D9] row-start-2 flex items-center justify-center">Khác</div>
   <div className="bg-[#D9D9D9] col-span-2 row-start-2 flex items-center justify-center">Blog</div>
