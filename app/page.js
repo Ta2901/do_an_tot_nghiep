fd4ddc90c142b8f74b41f28b6e3149a53f284c25
hiep2309 <larvahiep.com>
2024-12-04 00:45:28 +0900

    hiep 1

--- app/page.js
+++ app/page.js
@@ -9,15 +9,9 @@ export default function Page  () {
 
       <div className="grid  grid-rows-5 gap-4 h-screen mx-24  mt-8 max-w-7xl text-xl text-black">
   <div className="bg-[#D9D9D9] col-span-1 row-span-1 flex items-center justify-center">
-<<<<<<< HEAD
-  <img src="image1.jpg" alt="Tin tức" className="w-16 h-16 object-cover mb-2" />
-  <p>Tin tức</p> </div>
-  <div className="bg-[#D9D9D9] flex items-center justify-center">Pháp luật</div>
-=======
     
     Tin tức</div>
   <div className="bg-[#D9D9D9] flex items-center justify-center">Pháp luật ,mfngfdg</div>
->>>>>>> origin/main
   <div className="bg-[#D9D9D9] flex items-center justify-center">Du lịch</div>
   <div className="bg-[#D9D9D9] row-start-2 flex items-center justify-center">Khác</div>
   <div className="bg-[#D9D9D9] col-span-2 row-start-2 flex items-center justify-center">Blog</div>
