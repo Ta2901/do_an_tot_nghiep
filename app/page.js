dc0e5272443474f1e13dbd064544972a4b14d894
Ta2901 <tuananhnguyensi2901@gmail.com>
2024-12-04 00:36:13 +0900

    tuan

--- app/page.js
+++ app/page.js
@@ -9,8 +9,8 @@ export default function Page  () {
 
       <div className="grid  grid-rows-5 gap-4 h-screen mx-24  mt-8 max-w-7xl text-xl text-black">
   <div className="bg-[#D9D9D9] col-span-1 row-span-1 flex items-center justify-center">
-    
-    Tin tức</div>
+  <img src="image1.jpg" alt="Tin tức" className="w-16 h-16 object-cover mb-2" />
+  <p>Tin tức</p> </div>
   <div className="bg-[#D9D9D9] flex items-center justify-center">Pháp luật</div>
   <div className="bg-[#D9D9D9] flex items-center justify-center">Du lịch</div>
   <div className="bg-[#D9D9D9] row-start-2 flex items-center justify-center">Khác</div>
